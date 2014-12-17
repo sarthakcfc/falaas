6ed54ec37a24b039967728687200a7fc1794f18a
Sarthak Khatiwada <khatiwadas@wit.edu>
2014-12-16 22:54:52 -0500

    First Commit
    Uploading initial files

---
+++ Cards.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace falaas
+{
+    class Cards
+    {
+        public String rank { get; set; }
+        public String suit { get; set; }
+
+        public Cards(String rank, String suit)
+        {
+            this.rank = rank;
+            this.suit = suit;
+        }
+
+        public String print()
+        {
+            return (this.rank + " of " + this.suit);
+        }
+    }
+}
---
+++ main.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace falaas
+{
+    class main
+    {
+        public static void Main()
+        {
+            Cards c = new Cards("Ace", "Spades");
+            Console.WriteLine(c.print());
+        }
+    }
+}
