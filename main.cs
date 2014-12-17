3abc5d76891bf4fe654afdfea831c051d21404f4
aayush-pokharel <aayush.pokharel18@gmail.com>
2014-12-17 13:17:52 -0500

    Update main.cs

--- main.cs
+++ main.cs
@@ -11,14 +11,14 @@ namespace falaas
         public static void Main()
         {
             //This loop prints out the entire deck with 52 cards in order//
-            //The outer loop variable "i" on this test will serve as the rank.
-            for (int i = 1; i <= 13; i++)
+            //The outer loop variable on this test will serve as the rank.
+            for (int rank = 1; rank <= 13; rank++)
             {
                 //the inner loop variable serves as the suit number that gets passed in.
-                for (int j = 1; j <= 4; j++)
+                for (int suit = 1; suit <= 4; suit++)
                 {
                     //These staements will print out the deck into the console.
-                    Deck d = new Deck(i - 1, j - 1);
+                    Deck d = new Deck(rank - 1, suit - 1);
                     Cards c = new Cards(d.getRank(), d.getSuit());
                     Console.WriteLine(c.print());
                 }
@@ -28,6 +28,3 @@ namespace falaas
         }
     }
 }
-    }
-}
-
