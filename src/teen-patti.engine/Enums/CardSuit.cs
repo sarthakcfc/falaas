a351d31973d09f333eb547a3cb657269909a3d0d
Aayush Pokharel <apokharel@braveriver.com>
2022-07-17 15:30:07 -0400

    init project commit

---
+++ src/teen-patti.engine/Enums/CardSuit.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace teen_patti.engine
+{
+    public enum CardSuit
+    {
+        Daimond = 0,
+        Clover = 1,
+        Heart = 2,
+        Spade = 3
+    }
+    public static class CardSuitExtensions
+    {
+        public static string ToFriendlyString(this CardSuit suit) => suit switch
+        {
+            CardSuit.Daimond => "♦",
+            CardSuit.Clover => "♣",
+            CardSuit.Heart => "♥",
+            CardSuit.Spade => "♠",
+            _ => throw new Exception("Invalid Suit")
+        };
+    }
+}
